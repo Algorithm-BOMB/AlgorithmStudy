f821e3b1867dd66316b3c36abbd0148467f1d144
CheeseB <2489ckckck@naver.com>
2021-07-27 15:54:18 +0900

    약수의합 1,2 수정

--- seongyeon/basic_math_week_1/4_boj17427.js
+++ seongyeon/basic_math_week_1/4_boj17427.js
@@ -1,26 +1,9 @@
 const stdin = require('fs').readFileSync('/dev/stdin').toString();
 
 const n = Number(stdin);
-let gSum = 0;
-let fSum;
-
-// n 이하의 자연수
+let answer = 0;
 for (let i = 1; i <= n; i++) {
-  fSum = 0;
-
-  // 약수의 합 구하기
-  for (let j = 1; j * j <= i; j++) {
-    if (i % j === 0) {
-      fSum += j;
-      if (j * j < i) {
-        fSum += i / j;
-      }
-    }
-  }
-
-  gSum += fSum;
+  answer += Math.floor(n / i) * i;
 }
 
-console.log(gSum);
-
-// 시간초과
+console.log(answer);
--- seongyeon/basic_math_week_1/5_boj17425.js
+++ seongyeon/basic_math_week_1/5_boj17425.js
@@ -1,30 +1,21 @@
 const stdin = require('fs').readFileSync('/dev/stdin').toString();
 
-const input = stdin.split('\n').map(Number);
-let n;
-for (let test = 1; test < input.length; test++) {
-  n = input[test];
-  let gSum = 0;
-  let fSum;
+const LENGTH = 1000001;
+const nlist = stdin.split('\n').map(Number);
+let fList = new Array(LENGTH).fill(1);
+let gList = new Array(LENGTH).fill(0);
 
-  // n 이하의 자연수
-  for (let i = 1; i <= n; i++) {
-    fSum = 0;
-
-    // 약수의 합 구하기
-    for (let j = 1; j * j <= i; j++) {
-      if (i % j === 0) {
-        fSum += j;
-        if (j * j < i) {
-          fSum += i / j;
-        }
-      }
-    }
-
-    gSum += fSum;
+for (let i = 2; i < LENGTH; i++) {
+  for (let j = 1; i * j < LENGTH; j++) {
+    fList[i * j] += i;
   }
-
-  console.log(gSum);
+  gList[i] = gList[i - 1] + fList[i];
 }
 
-// 시간초과
+for (let i = 1; i < LENGTH; i++) {
+  gList[i] = gList[i - 1] + fList[i];
+}
+
+for (let i = 1; i < nlist.length; i++) {
+  console.log(gList[nlist[i]]);
+}
