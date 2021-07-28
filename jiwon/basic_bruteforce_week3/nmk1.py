313f0f6443dee93eb0c1d2b93dc23d4e8b43e7de
Yang JiWon <ambosing_@naver.com>
2021-07-28 21:50:49 +0900

    algorithm solving add

--- jiwon/basic_bruteforce_week3/nmk1.py
+++ jiwon/basic_bruteforce_week3/nmk1.py
@@ -1,6 +1,51 @@
 # N x M, K개를 선택, 선택한 칸에 들어 있는 수를 모두 더한 값의 최대값을 구하려고 한다.
 # 단, 선택한 두 칸이 인접하면 안된다. (상하좌우)
 
+# 풀이
+# 1. n, m, k, n x m의 board 입력
+# 2. dfs 구현
+# - 종료 조건 : k번 선택했을 때
+# - 반복 조건 : 대각선만 가능하게 (-1, -1), (1, 1), (-1, 1) (1, -1) -> 불가능.. 근처에 있는 대각선 값만 더하게 됨, 최적의 값 X
+# 3. 최대인 값과 변경
 
 
+# 틀렸음..
+# 트
 
+import sys
+
+d = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+n, m, k = map(int, sys.stdin.readline().split())
+b = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+answer = -sys.maxsize
+
+
+def can_visit(r, c, v):
+    for y, x in d:
+        nr = r + y
+        nc = c + x
+        if 0 <= nr < n and 0 <= nc < m and v[nr][nc]:
+            return False
+    return True
+
+
+def dfs(x, y, depth, v, val):
+    global answer
+    if depth == k:
+        answer = max(answer, val)
+        return
+    for r in range(y, n):
+        for c in range(x if y == r else 0, m):
+            if not v[r][c] and can_visit(r, c, v):
+                v[r][c] = True
+                dfs(c, r, depth + 1, v, val + b[r][c])
+                v[r][c] = False
+
+
+def solution():
+    v = [[False] * m for _ in range(n)]
+    dfs(0, 0, 0, v, 0)
+    print(answer)
+
+
+solution()
---
+++ jiwon/basic_bruteforce_week_2/tetromino.py
@@ -0,0 +1,58 @@
+# M, N의 보드
+# 회전, 대칭이 된다.
+
+# 풀이
+# 1. n, m, board 입력
+# 2. dfs로 테트로미노 구현, 길이가 4될 때 종료
+# 3. ㅏ, ㅓ, ㅗ, ㅜ는 기존의 dfs로는 불가능하므로, 따로 구현
+# 3-1. 회전할 수 있도록 하기, 리스트에 좌표 초기화
+# 4. 최댓값과 비교
+
+from sys import stdin
+
+
+a = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+aa = [[(0, -1), (-1, 0), (0, 1)], [(0, -1), (1, 0), (0, 1)], [(-1, 0), (1, 0), (0, 1)], [(-1, 0), (1, 0), (0, -1)]]
+answer = 0
+n, m = map(int, input().split())
+b = [list(map(int, stdin.readline().split())) for _ in range(n)]
+
+
+def dfs(r, c, v, val):
+    global answer
+    if len(v) == 4:
+        answer = max(answer, val)
+        return
+    for i in range(4):
+        nr = r + a[i][0]
+        nc = c + a[i][1]
+        if 0 <= nr < n and 0 <= nc < m and (nr, nc) not in v:
+            v.append((nr, nc))
+            dfs(nr, nc, v, val + b[nr][nc])
+            v.pop()
+
+
+def dfs2(r, c):
+    global answer
+    for t in aa:
+        val = b[r][c]
+        for xy in t:
+            nr = r + xy[0]
+            nc = c + xy[1]
+            if 0 <= nr < n and 0 <= nc < m:
+                val += b[nr][nc]
+            else:
+                break
+        answer = max(answer, val)
+
+
+def solution():
+    for r in range(n):
+        for c in range(m):
+            dfs(r, c, [(r, c)], b[r][c])
+            dfs2(r, c)
+    print(answer)
+
+
+solution()
+
