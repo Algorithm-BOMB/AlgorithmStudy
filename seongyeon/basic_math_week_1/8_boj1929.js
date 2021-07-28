b5f9bce9b5d9dd8c77a331a50dc5abb2f5d86691
CheeseB <2489ckckck@naver.com>
2021-07-28 15:27:02 +0900

    modify

--- seongyeon/basic_math_week_1/8_boj1929.js
+++ seongyeon/basic_math_week_1/8_boj1929.js
@@ -1,16 +1,19 @@
 const stdin = require('fs').readFileSync('/dev/stdin').toString();
 
-const LENGTH = 1000001;
 const [m, n] = stdin.split(' ').map(Number);
-let eratos = new Array(LENGTH).fill(0);
+let isPrimeNumber = new Array(n + 1).fill(true);
+let result = [];
 
-eratos[1] = 1;
-for (let i = 2; i < LENGTH && eratos[i] === 0; i++) {
-  for (let j = 2; i * j < LENGTH; j++) {
-    eratos[i * j] = 1;
-  }
+isPrimeNumber[1] = false;
+for (let i = 2; i <= n; i++) {
+  if (isPrimeNumber[i])
+    for (let j = i * 2; j <= n; j += i) {
+      isPrimeNumber[j] = false;
+    }
 }
 
 for (let i = m; i <= n; i++) {
-  if (eratos[i] === 0) console.log(i);
+  if (isPrimeNumber[i]) result.push(i);
 }
+
+console.log(result.join('\n'));
