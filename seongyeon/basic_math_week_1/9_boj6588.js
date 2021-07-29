3895a814c379a62daaa315e49b4deb739ae51e04
CheeseB <2489ckckck@naver.com>
2021-07-30 00:18:23 +0900

    골드바흐

--- seongyeon/basic_math_week_1/9_boj6588.js
+++ seongyeon/basic_math_week_1/9_boj6588.js
@@ -1,8 +1,5 @@
-// const stdin = require('fs').readFileSync('/dev/stdin').toString();
-const stdin = `8
-20
-42
-0`;
+const stdin = require('fs').readFileSync('/dev/stdin').toString();
+
 const input = stdin.split('\n').map(Number);
 const LENGTH = 1000000;
 let isPrimeNumber = new Array(LENGTH + 1).fill(true);
@@ -19,8 +16,8 @@ let n, primeNumber;
 let result = [];
 for (let i = 0; i < input.length - 1; i++) {
   n = input[i];
-  primeNumber = 3;
-  while (primeNumber > n - primeNumber) {
+  primeNumber = 2;
+  while (primeNumber <= n - primeNumber) {
     if (isPrimeNumber[n - primeNumber]) {
       result.push(n + ' = ' + primeNumber + ' + ' + (n - primeNumber));
       break;
