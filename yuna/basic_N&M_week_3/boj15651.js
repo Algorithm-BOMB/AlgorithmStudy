d483f9f6e6419f1100f1d573eaa9f3b24c4e4b16
yuna <ouna0223@naver.com>
2021-07-31 17:33:48 +0900

    15651_N&M(3)

---
+++ yuna/basic_N&M_week_3/boj15651.js
@@ -0,0 +1,37 @@
+/* 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+   - 1부터 N까지 자연수 중에서 M개를 고른 수열
+   - 같은 수를 여러 번 골라도 된다. */
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let list = [];
+let result = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", async function () {
+  input = input[0].split(" ");
+  let n = input[0];
+  let m = input[1];
+  dfs(0, n, m);
+  console.log(result.join("\n"));
+  process.exit();
+});
+
+let dfs = function (cnt, n, m, k) {
+  if (cnt == m) {
+    result.push(list.join(" "));
+    return 1;
+  }
+  for (let i = 1; i <= n; i++) {
+    list[cnt] = i;
+    k = i;
+    dfs(cnt + 1, n, m);
+  }
+  return 1;
+};
