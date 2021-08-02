0601ddac493182a33333e4eefb51a64a31cd8a76
CheeseB <2489ckckck@naver.com>
2021-08-02 22:42:20 +0900

    nm4

---
+++ seongyeon/basic_week3_nm/4_boj15652.js
@@ -0,0 +1,20 @@
+const stdin = require('fs').readFileSync('/dev/stdin').toString();
+
+const [n, m] = stdin.split(' ').map(Number);
+let arr = new Array(m);
+let result = '';
+
+function dfs(depth, start) {
+  if (depth === m) {
+    result += arr.join(' ') + '\n';
+    return;
+  }
+
+  for (let i = start; i <= n; i++) {
+    arr[depth] = i;
+    dfs(depth + 1, i);
+  }
+}
+
+dfs(0, 1);
+console.log(result);
