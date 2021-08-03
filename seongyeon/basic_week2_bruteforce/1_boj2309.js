bb1aed67d4db75ace49418ec117a35f8bc1afa01
CheeseB <2489ckckck@naver.com>
2021-08-04 00:53:53 +0900

    shit code

--- seongyeon/basic_week2_bruteforce/1_boj2309.js
+++ seongyeon/basic_week2_bruteforce/1_boj2309.js
@@ -9,19 +9,30 @@ const stdin = `20
 8
 13`;
 
-const nums = stdin.split('\n').map(Number);
+const nums = stdin
+  .split('\n')
+  .map(Number)
+  .sort((a, b) => {
+    return a - b;
+  });
 let arr = new Array(7);
 let sum = 0;
+let isFound = false;
 
-function findDwarf(depth, index, remain) {
-  if (remain === 0) {
-    //blabla
+function findDwarf(depth, index) {
+  if (isFound) return;
+
+  if (depth === 7) {
+    if (sum === 100) {
+      isFound = true;
+      console.log(arr.join('\n'));
+    }
   } else if (index === 9) return;
   else {
     arr[depth] = nums[index];
-    findDwarf(depth + 1, index + 1, remain - 1);
-    findDwarf(depth, index + 1, remain);
+    findDwarf(depth + 1, index + 1);
+    findDwarf(depth, index + 1);
   }
 }
 
-findDwarf(0, 0, 7);
+findDwarf(0, 0);
