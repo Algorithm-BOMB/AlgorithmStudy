47eefd5c26ddfe07150186f6c944ca439782934e
CheeseB <2489ckckck@naver.com>
2021-08-03 14:16:55 +0900

    modify

--- seongyeon/basic_week1_math/5_boj17425.js
+++ seongyeon/basic_week1_math/5_boj17425.js
@@ -1,7 +1,7 @@
 const stdin = require('fs').readFileSync('/dev/stdin').toString();
 
 const LENGTH = 1000001;
-const nlist = stdin.split('\n').map(Number);
+const nList = stdin.split('\n').map(Number);
 let fList = new Array(LENGTH).fill(1);
 let gList = new Array(LENGTH).fill(0);
 
@@ -15,6 +15,9 @@ for (let i = 1; i < LENGTH; i++) {
   gList[i] = gList[i - 1] + fList[i];
 }
 
-for (let i = 1; i < nlist.length; i++) {
-  console.log(gList[nlist[i]]);
+let result = '';
+for (let i = 1; i <= nList[0]; i++) {
+  result += gList[nList[i]] + '\n';
 }
+
+console.log(result);
