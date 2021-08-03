29244d80529eae8ee35cc7a23cae5e5569eba49e
CheeseB <2489ckckck@naver.com>
2021-08-03 17:53:32 +0900

    a

--- seongyeon/basic_week2_bruteforce/1_boj2309.js
+++ seongyeon/basic_week2_bruteforce/1_boj2309.js
@@ -1,1 +1,27 @@
-// uhh
+// const stdin = require('fs').readFileSync('/dev/stdin').toString();
+const stdin = `20
+7
+23
+19
+10
+15
+25
+8
+13`;
+
+const nums = stdin.split('\n').map(Number);
+let arr = new Array(7);
+let sum = 0;
+
+function findDwarf(depth, index, remain) {
+  if (remain === 0) {
+    //blabla
+  } else if (index === 9) return;
+  else {
+    arr[depth] = nums[index];
+    findDwarf(depth + 1, index + 1, remain - 1);
+    findDwarf(depth, index + 1, remain);
+  }
+}
+
+findDwarf(0, 0, 7);
