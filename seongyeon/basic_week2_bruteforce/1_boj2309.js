e6199bb9abf887651143556d2e7e7546a4d8920b
CheeseB <2489ckckck@naver.com>
2021-08-05 10:31:51 +0900

    wrong code

--- seongyeon/basic_week2_bruteforce/1_boj2309.js
+++ seongyeon/basic_week2_bruteforce/1_boj2309.js
@@ -1,13 +1,13 @@
 // const stdin = require('fs').readFileSync('/dev/stdin').toString();
-const stdin = `20
-7
-23
-19
-10
-15
-25
-8
-13`;
+const stdin = `32
+2
+79
+4
+5
+6
+3
+1
+10`;
 
 const nums = stdin
   .split('\n')
@@ -30,7 +30,9 @@ function findDwarf(depth, index) {
   } else if (index === 9) return;
   else {
     arr[depth] = nums[index];
+    sum += nums[index];
     findDwarf(depth + 1, index + 1);
+    sum -= nums[index];
     findDwarf(depth, index + 1);
   }
 }
