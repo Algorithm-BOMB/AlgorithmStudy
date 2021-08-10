01fccc6d0b593c7613641be43c246764b1dc93de
CheeseB <2489ckckck@naver.com>
2021-08-10 17:28:52 +0900

    nmk

---
+++ seongyeon/basic_week3_nm/9_boj18290.js
@@ -0,0 +1,15 @@
+// const stdin = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const stdin = `5 5 3
+1 9 8 -2 0
+-1 9 8 -3 0
+-5 1 9 -1 0
+0 0 0 9 8
+9 9 9 0 0`;
+const input = stdin.split('\n');
+const [n, m, k] = input[0].split(' ').map(Number);
+let grid = new Array(n);
+for (let i = 1; i <= n; i++) grid[i - 1] = input[i].split(' ').map(Number);
+
+console.log(n, m, k);
+console.log(grid);
