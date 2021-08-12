c491ab43be2eb3f21165ec7c888b0f668690b08d
CheeseB <2489ckckck@naver.com>
2021-08-12 14:34:13 +0900

    nmk

--- seongyeon/basic_week3_nm/9_boj18290.js
+++ seongyeon/basic_week3_nm/9_boj18290.js
@@ -1,26 +1,44 @@
-// const stdin = require('fs').readFileSync('/dev/stdin').toString().trim();
+const stdin = require('fs').readFileSync('/dev/stdin').toString().trim();
 
-const stdin = `5 5 3
-1 9 8 -2 0
--1 9 8 -3 0
--5 1 9 -1 0
-0 0 0 9 8
-9 9 9 0 0`;
 const input = stdin.split('\n');
-const [n, m, k] = input[0].split(' ').map(Number);
-let grid = new Array(n);
-for (let i = 1; i <= n; i++) grid[i - 1] = input[i].split(' ').map(Number);
+const [N, M, K] = input[0].split(' ').map(Number);
+let grid = new Array(N);
+for (let i = 1; i <= N; i++) grid[i - 1] = input[i].split(' ').map(Number);
 
 let max = -40000;
-let border = new Array(n);
+let trap = new Array(N);
+for (let i = 0; i < N; i++) trap[i] = new Array(M).fill(0);
 
-const x = [0, 0, 1, -1];
-const y = [-1, 1, 0, 0];
-function getMaxK(sum, pick, i, j) {
-  if (pick === 0) {
-    if (sum > max) max = sum;
-    return;
+function check(i, j) {
+  trap[i][j]++;
+  if (i > 0) trap[i - 1][j]++;
+  if (i < N - 1) trap[i + 1][j]++;
+  if (j > 0) trap[i][j - 1]++;
+  if (j < M - 1) trap[i][j + 1]++;
+}
+
+function uncheck(i, j) {
+  trap[i][j]--;
+  if (i > 0) trap[i - 1][j]--;
+  if (i < N - 1) trap[i + 1][j]--;
+  if (j > 0) trap[i][j - 1]--;
+  if (j < M - 1) trap[i][j + 1]--;
+}
+
+function dfs(pick, sum) {
+  if (pick === K) {
+    if (max < sum) max = sum;
+  } else {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < M; j++) {
+        if (trap[i][j] > 0) continue;
+        check(i, j);
+        dfs(pick + 1, sum + grid[i][j]);
+        uncheck(i, j);
+      }
+    }
   }
 }
 
-getMaxK(0, k, 0, 0);
+dfs(0, 0);
+console.log(max);
