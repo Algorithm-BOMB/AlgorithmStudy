4e30da395852c5993da1384a65c8523b8d15a85f
Yang Jiwon <ambosing_@naver.com>
2021-08-13 17:10:33 +0900

    java algorithm add

--- jiwon/basic_permutation_week5/piece of paper/src/Main.java
+++ jiwon/basic_permutation_week5/piece of paper/src/Main.java
@@ -14,24 +14,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+    static int[][] board;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split("\\s");
         int n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
         int nm = n * m;
-        int res = 0;
-        for (int i = 0; i < (1 << nm); i++) {
-            int sum = 0;
-            for (int j = 0; j < m; j++) {
-                int cur = 0;
-
-            }
-
+        board = new int[n][m];
+        for (int i = 0; i < n; i++)
+            board[i] = Arrays.stream(br.readLine().split(""))
+                    .mapToInt(Integer::parseInt).toArray();
+        int result = 0;
+        int a = 0;
+        for (int s = 0; s < (1 << nm); s++) {
+           int sum = pieceOfPaper(s, n, m, a++);
+           sum += pieceOfPaper(s, m, n, a++);
+           result = Math.max(result, sum);
         }
+        System.out.println(result);
+    }
 
+    static int pieceOfPaper(int s, int row, int col, int a) {
+        int sum = 0;
+        for (int i = 0; i < row; i++) {
+            int cur = 0;
+            for (int j = 0; j < col; j++) {
+                if (a % 2 == 0) {
+                    int k = i * col + j;
+                    if ((s & (1 << k)) == 0)
+                        cur = cur * 10 + board[i][j];
+                    else {
+                        sum += cur;
+                        cur = 0;
+                    }
+                } else {
+                    int k = j * row + i;
+                    if ((s & (1 << k)) != 0)
+                        cur = cur * 10 + board[j][i];
+                    else {
+                        sum += cur;
+                        cur = 0;
+                    }
+                }
+            }
+            sum += cur;
+        }
+        return sum;
     }
 }
