a48ea500bae2832f89b70ec1efb126f6eb0748fd
CheeseB <2489ckckck@naver.com>
2021-08-17 16:40:48 +0900

    make 1

--- seongyeon/basic_week6_dp1/1_boj1463.js
+++ seongyeon/basic_week6_dp1/1_boj1463.js
@@ -1,1 +1,14 @@
-const stdin = require('fs').readFileSync('/dev/stdin').toString();
+const stdin = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const N = Number(stdin);
+let dp = new Array(N + 1);
+dp[1] = 0;
+
+for (let i = 2; i <= N; i++) {
+  dp[i] = dp[i - 1] + 1;
+
+  if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+}
+
+console.log(dp[N]);
