37e0a940463a735ae76fdb9b81982067d9848bc4
Yang Jiwon <ambosing_@naver.com>
2021-09-03 23:52:02 +0900

    python code add

--- jiwon/Kakao_test/Change braket.py
+++ jiwon/Kakao_test/Change braket.py
@@ -1,12 +1,43 @@
-'''
-(, )의 개수가 같다면 균형잡힌 괄호 문자열
-(, )의 괄호의 짝도 모두 맞을 경우는 올바른 괄호 문자열
+def is_right(s):
+    a = []
+    for c in s:
+        if a and c == ")":
+            a.pop()
+        elif c == "(":
+            a.append(c)
+        else:
+            return False
+    if a:
+        return False
+    return True
 
-생각
-- 올바른 괄호 문자열인지 확인
-- 아니라면, 옆의 과정을 수행하는데..
-- 나누는 기준은 균형잡힌 괄호 문자열
-'''
+
+def get_balanced(p):
+    if not p:
+        return
+    e = 0
+    u = ""
+    while True:
+        u = p[0:e]
+        v = p[e:]
+        if u.count('(') != u.count(')'):
+            break
+        e += 2
+    return u, v
+
+
+def reverse_braket(p):
+
+
+
 def solution(p):
     answer = ''
+    if not p or is_right(p):
+        return p
+    else:
+        u, v = get_balanced(p)
+        if is_right(u):
+            answer = u * solution(v)
+        else:
+            answer = "(" + solution(v) + ")" + u[1:-1]
     return answer
